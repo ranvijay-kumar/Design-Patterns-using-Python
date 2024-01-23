6237102206cfeabf753f89146449320b9d92825a
ranvijay <mailkumarranvijay@gmail.com>
2024-01-23 16:10:08 +0530

    liskov substitution principle

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 ## SOLID Principles
 ### S: Single Responsibility Principle
 `Single Responsibility Principle states that a class should only have one reason to change. Also tied to the concept of Separation of Concerns which is basically stating the same thing.`
+
+### O: Open-Closed Principle
+`Open-Closed Principle states that classes should be open for extension, but closed for modification. In other words, you should extend functionality using interfaces and inheritance rather than jumping back into already-written/tested code and adding to it or changing it.`
+
+### L: Liskov Substitution Principle
+`The Liskov Substitution Principle states that subtypes should be substitutable for their base types.`
+
+### I: Interface Segregation Principle
+`The Interface Segregation Principle is simple: don't throw everything in the kitchen sink into an interface because then all its users will have to implement things they do not need. Instead, split the interface into several smaller ones.`
+
+### D: Dependency Inversion Principle
+`Not to be confused with dependency injection, dependency inversion specifies that high-level modules should not depend on low-level ones; both should depend on abstractions.`
+
+
+## Builder
---
+++ SOLID/Liskov Substitution Principle.py
@@ -0,0 +1,58 @@
+class Rectangle:
+    def __init__(self, width, height) -> None:
+        self._height=height
+        self._width=width
+    
+    @property
+    def width(self):
+        return self._width
+    
+    @width.setter
+    def width(self,value):
+        self._width=value
+
+    @property
+    def height(self):
+        return self._height
+    
+    @height.setter
+    def height(self,height):
+        self._height=height
+    
+    @property
+    def area(self):
+        return self._width*self._height
+    
+    def __str__(self) -> str:
+        return f"Width: {self.width}, height: {self.height}"
+    
+class Square(Rectangle):
+    def __init__(self, size) -> None:
+        super().__init__(size, size)
+    
+    # breaks liskov substitution principle
+    @Rectangle.width.setter
+    def width(self,value):
+        self._width=self._height=value
+    
+    # breaks liskov substitution principle
+    @Rectangle.height.setter
+    def height(self,value):
+        self._width=self._height=value
+
+
+
+
+def use_it(rc: Rectangle):
+    w=rc.width
+    rc.height=10
+    expected = int(w*10)
+    print(f"Expected an area of {expected}, got {rc.area}")
+
+rc= Rectangle(2,3)
+use_it(rc)
+
+sq = Square(5)
+use_it(sq)
+
+
