b2de80583c5eacd582ba99f02a4cf7665d7e1b24
ranvijay-kumar <mailkumarranvijay@gmail.com>
2025-01-20 10:52:56 +0530

    Interpreter design pattern

---
+++ Interpreter/exercise.py
@@ -0,0 +1,32 @@
+import re
+import unittest
+from enum import Enum
+
+
+class ExpressionProcessor:
+    def __init__(self):
+        self.variables = {}
+
+    def calculate(self, expression):
+        # todo
+        pass
+
+
+class FirstTestSuite(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        ep = ExpressionProcessor()
+        ep.variables['x'] = 5
+        cls.ep = ep
+
+    def test_simple(self):
+        self.assertEqual(1, self.ep.calculate('1'))
+
+    def test_addition(self):
+        self.assertEqual(3, self.ep.calculate('1+2'))
+
+    def test_addition_with_variable(self):
+        self.assertEqual(6, self.ep.calculate('1+x'))
+
+    def test_failure(self):
+        self.assertEqual(0, self.ep.calculate('1+xy'))
